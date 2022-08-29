e6ab6313742a27fff3ebc5eb377e69a40a0b5a6a
epppo <amorosso@kakao.com>
2022-08-29 20:55:42 +0900

    FP-238 SVG import  재설정 compornent 타입으로 인식될 수 있도록 변경

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,8 @@
-declare module "*.svg" {
-  const content: any;
-  export default content;
+declare module '*.svg' {
+	import React = require('react');
+  
+  export const ReactComponent: REact.FC<React.SVGProps<SVGSVGElement>>;
+  // .svg 확장자의 파일에서 ReactComponent의 존재를 인식시켜 주는 부분
+  const src: string;
+	export default src;
 }
--- src/styles/assets/svg/index.tsx
+++ src/styles/assets/svg/index.tsx
@@ -1,8 +1,8 @@
-import HeaderLogo from './headerLogo.svg';
-import HeaderHam from './headerHam.svg';
+import { ReactComponent as HeaderLogo }from './headerLogo.svg';
+import { ReactComponent as HeaderHam } from './headerHam.svg';
 
-import LeftArrowBlack from './leftArrowBlack.svg';
-import RightArrowBlack from './rightArrowBlack.svg';
+import { ReactComponent as LeftArrowBlack } from './leftArrowBlack.svg';
+import { ReactComponent as RightArrowBlack } from './rightArrowBlack.svg';
 
 export { HeaderLogo, HeaderHam, LeftArrowBlack, RightArrowBlack }
 

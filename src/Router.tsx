2ff5054506c36d660d031d0c71a846564e521376
epppo <amorosso@kakao.com>
2022-08-30 22:29:27 +0900

    Create : 리뉴얼중 페이지 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import {
   ChooseTaesan,
   Community,
@@ -9,20 +9,22 @@ import {
   MyPage,
   Ranking,
   RealtimeChatting,
-} from "./page";
+  Notice,
+} from './page';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<DailyTikkeeul />} />
-      <Route path="/choosetaesan" element={<ChooseTaesan />} />
-      <Route path="/community" element={<Community />} />
-      <Route path="/detailpost" element={<DetailPost />} />
-      <Route path="/findpassword" element={<FindPassword />} />
-      <Route path="/loginpage" element={<LoginPage />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/ranking" element={<Ranking />} />
-      <Route path="/realtimechatting" element={<RealtimeChatting />} />
+      <Route path='/' element={<Notice />} />
+      <Route path='/dailytikkeeul' element={<DailyTikkeeul />} />
+      <Route path='/choosetaesan' element={<ChooseTaesan />} />
+      <Route path='/community' element={<Community />} />
+      <Route path='/detailpost' element={<DetailPost />} />
+      <Route path='/findpassword' element={<FindPassword />} />
+      <Route path='/loginpage' element={<LoginPage />} />
+      <Route path='/mypage' element={<MyPage />} />
+      <Route path='/ranking' element={<Ranking />} />
+      <Route path='/realtimechatting' element={<RealtimeChatting />} />
     </Routes>
   );
 };
--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,12 +1,13 @@
-import ChooseTaesan from "./ChooseTaesan";
-import Community from "./Community";
-import DailyTikkeeul from "./DailyTikkeeul";
-import DetailPost from "./DetailPost";
-import FindPassword from "./FindPassword";
-import LoginPage from "./LoginPage";
-import MyPage from "./MyPage";
-import Ranking from "./Ranking";
-import RealtimeChatting from "./RealtimeChatting";
+import ChooseTaesan from './ChooseTaesan';
+import Community from './Community';
+import DailyTikkeeul from './DailyTikkeeul';
+import DetailPost from './DetailPost';
+import FindPassword from './FindPassword';
+import LoginPage from './LoginPage';
+import MyPage from './MyPage';
+import Ranking from './Ranking';
+import RealtimeChatting from './RealtimeChatting';
+import Notice from './Notice';
 
 export {
   ChooseTaesan,
@@ -18,4 +19,5 @@ export {
   DetailPost,
   FindPassword,
   Ranking,
+  Notice,
 };

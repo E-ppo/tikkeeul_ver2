546a7028d58eb1ec9299006af65c444ba9dafb96
epppo <amorosso@kakao.com>
2022-08-30 15:27:25 +0900

    FP-238 글로벌스타일 수정중

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled, { GlobalStyleComponent } from 'styled-components';
 import Router from './Router';
 
 function App() {
   return (
+    <>
     <Router/>
+    </>
   );
 }
 export default App;

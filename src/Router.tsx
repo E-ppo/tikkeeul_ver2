70fa9ef07abbfb5785e8849116c5cd748d5fe3bb
epppo <amorosso@kakao.com>
2022-08-30 17:46:17 +0900

    tsconfig.json   import src 전체영역으로 설정변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import {
   ChooseTaesan,
   Community,
@@ -13,19 +13,17 @@ import {
 
 const Router = () => {
   return (
-      <Routes>
-        <Route path="/" element={<DailyTikkeeul />} />
-        <Route path="/choosetaesan" element={<ChooseTaesan />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/detailpost" element={<DetailPost />} />
-        <Route path="/findpassword" element={<FindPassword />} />
-        <Route path="/loginpage" element={<LoginPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/ranking" element={<Ranking />} />
-        <Route path="/realtimechatting" element={<RealtimeChatting />} />
-      </Routes>
-  )
-
-}
-export default Router
-
+    <Routes>
+      <Route path="/" element={<DailyTikkeeul />} />
+      <Route path="/choosetaesan" element={<ChooseTaesan />} />
+      <Route path="/community" element={<Community />} />
+      <Route path="/detailpost" element={<DetailPost />} />
+      <Route path="/findpassword" element={<FindPassword />} />
+      <Route path="/loginpage" element={<LoginPage />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/ranking" element={<Ranking />} />
+      <Route path="/realtimechatting" element={<RealtimeChatting />} />
+    </Routes>
+  );
+};
+export default Router;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <ThemeProvider theme={theme}>

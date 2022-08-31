abb039b88c412f523eb1ba5340eaa29e75c7d7a7
epppo <amorosso@kakao.com>
2022-08-31 22:28:46 +0900

    FP-238 Update : TotalTikkeeulPrice 컴포넌트 필수프로퍼티 지정 (토탈프라이스금액) / 폰트 사이즈와 컴포넌트 크기는 선택적으로 적용

--- src/page/DailyTikkeeul.tsx
+++ src/page/DailyTikkeeul.tsx
@@ -1,14 +1,27 @@
 import styled from 'styled-components';
+import { TikkeeulData } from '../data';
 import { Header, TotalTikkeeulPrice } from '../components/common';
 import { DayTikkeeulList, Teasan } from '../components/tikkeeul';
+import { MainAddButton } from '../styles/assets/svg/common';
 
 function DailyTikkeeul() {
+  const addMenu = () => {};
   return (
     <Wrap>
       <Header title={'데일리 티끌'} />
       <Teasan />
-      <TotalTikkeeulPrice fontSize={'1.2'} />
+      <TotalTikkeeulPrice
+        totalPrice={TikkeeulData.totalPrice} //꼭 있어야하는 필수 프로퍼티
+        fontSize={'1.2'} // 없어도 되는거 (없으면 1rem)
+        divHight={'50'} // 없어도 되는거(px)
+      />
       <DayTikkeeulList />
+      <ButtonFixed
+        onClick={() => {
+          addMenu();
+        }}>
+        <MainAddButton />
+      </ButtonFixed>
     </Wrap>
   );
 }
@@ -16,4 +29,11 @@ export default DailyTikkeeul;
 
 const Wrap = styled.div`
   width: 100%;
+`;
+
+const ButtonFixed = styled.div`
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  cursor: pointer;
 `;

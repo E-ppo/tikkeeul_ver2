e4709a4ba45bbbf1a623d3075f1338fb8f49f853
epppo <amorosso@kakao.com>
2022-08-31 22:26:43 +0900

    FP-238 Create : 나의 티끌목록 조회 컴포넌트 완

--- src/components/tikkeeul/index.tsx
+++ src/components/tikkeeul/index.tsx
@@ -1,5 +1,6 @@
 import DayTikkeeulList from './DayTikkeeulList';
 import Teasan from './Teasan';
 import FavoriteTag from './FavoriteTag';
+import LoadTikkeeulList from './LoadTikkeeulList';
 
-export { DayTikkeeulList, Teasan, FavoriteTag };
+export { DayTikkeeulList, Teasan, FavoriteTag, LoadTikkeeulList };

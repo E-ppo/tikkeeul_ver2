042a26bbc39a935ae944dd98c0a4af1922435e7d
epppo <amorosso@kakao.com>
2022-09-02 01:16:21 +0900

    FP-238 Refactor : DailyTikkeeul 페이지의 일부 컴포넌트 > MainTikkeeul로 분리하여 컴포넌트화

--- src/components/tikkeeul/index.tsx
+++ src/components/tikkeeul/index.tsx
@@ -2,5 +2,14 @@ import DayTikkeeulList from './DayTikkeeulList';
 import Teasan from './Teasan';
 import FavoriteTag from './FavoriteTag';
 import LoadTikkeeulList from './LoadTikkeeulList';
+import MainTikkeeul from './MainTikkeeul';
+import AddTikkeeul from './AddTikkeeul';
 
-export { DayTikkeeulList, Teasan, FavoriteTag, LoadTikkeeulList };
+export {
+  DayTikkeeulList,
+  Teasan,
+  FavoriteTag,
+  LoadTikkeeulList,
+  MainTikkeeul,
+  AddTikkeeul,
+};

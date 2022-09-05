3dca4f2aae53cf1fff9f3e60ada16164f264295a
sparrowscout <sparrowscout.0000@gmail.com>
2022-09-05 11:50:58 +0900

    Update : mypage

--- src/page/MyPage.tsx
+++ src/page/MyPage.tsx
@@ -1,4 +1,40 @@
+import styled from 'styled-components';
+import Header from '../components/common/Header';
+
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <>
+      <Header title={'MY'} />
+      <ProfileWrap>
+        <ProfileImg />
+        <div>
+          <span>지철</span>님<br /> 환영합니다!
+        </div>
+      </ProfileWrap>
+      <div>프로필 편집</div>
+      <div>로그아웃</div>
+      <div>즐겨찾기</div>
+    </>
+  );
 }
+const ProfileWrap = styled.div`
+  margin: 10px 30px;
+  text-align: center;
+  font-family: 'SEBANG_Gothic_Regular';
+  font-size: 1.5rem;
+  line-height: 36px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  span {
+    color: ${(props) => props.theme.mainMintColor};
+  }
+`;
+const ProfileImg = styled.div`
+  margin: auto;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: gray;
+`;
 export default MyPage;
